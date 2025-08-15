773208cccc8f41224eb16147efeef20abc9f6be8
Felipe Fama <lipehfama.dev@gmail.com>
2025-08-15 11:55:07 -0300

    feat: add vite file and settings pwa

---
+++ vite.config.js
@@ -0,0 +1,51 @@
+import { defineConfig } from "vite";
+
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "to do list",
+        short_name: "to do list",
+        description: "to do list app",
+        theme_color: "#fff",
+        start_url: "/",
+        display: "standalone",
+        lang: "en",
+        scope: "/",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
